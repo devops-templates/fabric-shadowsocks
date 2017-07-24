5eb1ddfd79c7e183ffb3339d4811c7f7d9daf63c
thisiswangle <wangle.tju@gmail.com>
2017-07-24 11:47:54 +0800

    add docker

---
+++ fabric_templates/docker-ce.py
@@ -0,0 +1,30 @@
+#coding=utf-8
+
+import os
+
+from fabric.api import *
+from fabric.contrib.files import *
+from fabric.colors import green as _info
+
+def install():
+    # Uninstall old versions
+    sudo("apt-get remove docker docker-engine docker.io")
+    sudo("apt-get update")
+    # Only for ubuntu 14.04
+    # sudo("apt-get install -y \
+    #        linux-image-extra-$(uname -r) \
+    #        linux-image-extra-virtual")
+    sudo("apt-get install -y \
+            apt-transport-https \
+            ca-certificates \
+            curl \
+            software-properties-common")
+    run("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
+    sudo("apt-key fingerprint 0EBFCD88")
+    sudo('add-apt-repository \
+            "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) \
+            stable"')
+    sudo("apt-get update")
+    sudo("apt-get install docker-ce")
+

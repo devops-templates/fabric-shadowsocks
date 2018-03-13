e6a26db6bd4bcc55d3a9057ed2e4a7732f53f851
thisiswangle <wangle.tju@gmail.com>
2018-03-14 00:02:16 +0800

    Update docker compose config

--- fabric_templates/docker_shadowsocks_libev.py
+++ fabric_templates/docker_shadowsocks_libev.py
@@ -15,24 +15,27 @@ services:
       - {1}:8388/tcp
       - {1}:8388/udp
     environment:
-      - METHOD="{2}"
-      - PASSWORD="{0}"
-      - OBFS_OPTS="obfs=http;obfs-host=www.bing.com"
+      - METHOD={2}
+      - PASSWORD={0}
+      - OBFS_OPTS=obfs=http;obfs-host=www.bing.com
     restart: always
 """
 
 def install():
     with cd('~'):
-        sudo("git clone https://github.com/devops-templates/docker-shadowsocks-libev.git shadowsocks")
+        sudo("git clone https://github.com/devops-templates/docker-shadowsocks-libev.git /opt/shadowsocks")
 
 def config(password="1234567890", port=10010, method="aes-192-cfb"):
-    with cd('~/shadowsocks'):
-        conffile_content = docker_compose_config.format(password, port, method)
-        with open('docker-compose.yml', 'w') as confile:
-            confile.write(conffile_content)
+    remote_config_path = '/opt/shadowsocks/docker-compose.yml'
+    local_config_path = tempfile.mktemp('.yml')
+    conffile_content = docker_compose_config.format(password, port, method)
+    with open(local_config_path, 'w') as confile:
+        confile.write(conffile_content)
+
+    put(local_config_path, remote_config_path, use_sudo=True, mode="644")
 
 def up():
-    with cd('~/shadowsocks'):
+    with cd('/opt/shadowsocks'):
         sudo('docker-compose up -d')
 
 

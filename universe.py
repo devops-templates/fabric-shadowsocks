a982ef4119a38cfec98d6d4cc84c7236924977da
thisiswangle <wangle.tju@gmail.com>
2018-09-17 16:25:15 +0800

    添加支持 hostus kvm 虚拟机自动化部署脚本

---
+++ universe.py
@@ -0,0 +1,33 @@
+#coding=utf8
+from fabric_templates import bbr
+from fabric_templates import common_utils
+from fabric_templates import docker_ce
+from fabric_templates import docker_compose
+from fabric_templates import docker_shadowsocks_libev
+
+
+def install():
+    common_utils.install()
+    docker_ce.install()
+    docker_compose.install()
+
+def upgrade_kernel():
+    bbr.upgrade_kernel()
+
+def enable_bbr():
+    bbr.config()
+
+def config(port, password):
+    docker_shadowsocks_libev.config(port, password)
+
+def start(port):
+    docker_shadowsocks_libev.up(port)
+
+def restart(port):
+    docker_shadowsocks_libev.restart(port)
+
+def setup(port="10010", password="wifjzgkfOObuonL2"):
+    config(port, password)
+    start(port)
+
+
